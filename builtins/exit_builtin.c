f007a5424b20bf71d2e7ef13799f82e2457ec5b6
SamyBravy <you@example.com>
2024-08-14 11:54:29 +0200

    aggiunto controllo long long int su exit

--- builtins/exit_builtin.c
+++ builtins/exit_builtin.c
@@ -12,26 +12,60 @@
 
 #include "../minishell.h"
 
-int	is_number(char *str)
+static int	is_number(char *str)
 {
 	int	i;
 
-	i = 0;
-	while (str[i])
+	i = -1;
+	while (str[++i])
 	{
 		if (!ft_isdigit(str[i])
 			&& !((str[i] == '-' || str[i] == '+') && i == 0))
 			return (0);
-		i++;
 	}
 	if (i == 1 && (str[0] == '-' || str[0] == '+'))
 		return (0);
+	if (str[0] == '+')
+	{
+		if (ft_strcmp(str, "+9223372036854775807") > 0)
+			return (0);
+	}
+	else if (str[0] == '-')
+	{
+		if (ft_strcmp(str, "-9223372036854775808") > 0)
+			return (0);
+	}
+	else
+		if (ft_strcmp(str, "9223372036854775807") > 0)
+			return (0);
 	return (1);
+}
+
+static long long int	ft_long_long_atoi(const char *nptr)
+{
+	int				i;
+	int				n;
+	long long int	res;
+
+	n = 0;
+	i = 1;
+	res = 0;
+	while ((nptr[n] >= 9 && nptr[n] <= 13) || nptr[n] == ' ')
+		n += 1;
+	if (nptr[n] == '-' || nptr[n] == '+')
+	{
+		if (nptr[n] == '-')
+			i *= -1;
+		n += 1;
+	}
+	while (nptr[n] >= '0' && nptr[n] <= '9')
+		res = res * 10 + (nptr[n++] - '0');
+	return (res * i);
 }
 
 int	exit_builtin(char **argv, t_input **input)
 {
-	int	exit_status;
+	long long int	exit_status;
 
 	ft_putstr_fd("exit\n", STDERR_FILENO);
 	if (!argv[1])
@@ -48,7 +82,7 @@ int	exit_builtin(char **argv, t_input **input)
 				STDERR_FILENO), 1);
 	else
 	{
-		exit_status = ft_atoi(argv[1]);
+		exit_status = ft_long_long_atoi(argv[1]);
 		if (exit_status < 0)
 			exit_status = 256 - ((-exit_status) % 256);
 		exit_status %= 256;
