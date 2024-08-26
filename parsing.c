0d334e3a4f72f4ace88d3a54a6c38c028c4f69d4
filippo <filippo@ubuntu.myguest.virtualbox.org>
2024-08-26 17:37:03 +0200

    sintax error sitemati

--- parsing.c
+++ parsing.c
@@ -3,14 +3,14 @@
 
 t_type which_token(char *token)
 {
-    if (ft_strncmp(token, ">", 1) == 0)
-        return TRUNC;
     if (ft_strncmp(token, ">>", 2) == 0)
         return APPEND;
-    if (strncmp(token, "<", 1) == 0)
-        return INPUT;
     if (strncmp(token, "<<", 2) == 0)
         return HEREDOC;
+    if (ft_strncmp(token, ">", 1) == 0)
+        return TRUNC;
+    if (strncmp(token, "<", 1) == 0)
+        return INPUT;
     if (strncmp(token, "|", 1) == 0)
         return PIPE;
     return CMD;
@@ -57,14 +57,37 @@ void	jump_space_and_quotes(char **str)
 	while (jump_space(str) || jump_quotes(str))
 		;
 }
+
+int control_next_token(char **token)	
+{
+	if (which_token(*token) == HEREDOC || which_token(*token) == APPEND)
+		*token += 2;
+    else
+	    (*token)++;
+	jump_space(token);
+	if (which_token(*token) == PIPE)
+		return syntax_error("|");
+	if (which_token(*token) == HEREDOC)
+		return syntax_error("<<");
+	if (which_token(*token) == APPEND)
+		return syntax_error(">>");
+	if (which_token(*token) == INPUT)
+		return syntax_error("<");
+	if (which_token(*token) == TRUNC)
+		return syntax_error(">");
+	if (which_token(*token) == CMD && !**token)
+		return syntax_error("newline");
+	return 0;
+}
+
 int check_syntax_errors(char *token)
 {
 	jump_space(&token);
 	if(which_token(token) == PIPE)
 		return syntax_error("|");
-	jump_space_and_quotes(&token);
 	while(*token)
 	{
+		jump_space_and_quotes(&token);
 		if(which_token(token) == PIPE)
 		{
 			token++;
@@ -75,26 +98,11 @@ int check_syntax_errors(char *token)
 		else if(which_token(token) == HEREDOC || which_token(token) == APPEND
 			|| which_token(token) == INPUT || which_token(token) == TRUNC)
 		{
-			token++;
-			if (which_token(token) == HEREDOC || which_token(token) == APPEND)
-				token++;
-			jump_space(&token);
-			if (which_token(token) == PIPE)
-				return syntax_error("|");
-			if (which_token(token) == HEREDOC)
-				return syntax_error("<<");
-			if (which_token(token) == APPEND)
-				return syntax_error(">>");
-			if (which_token(token) == INPUT)
-				return syntax_error("<");
-			if (which_token(token) == TRUNC)
-				return syntax_error(">");
-			if (which_token(token) == CMD)
-				return syntax_error("newline");
+			if (control_next_token(&token))
+				return (-1);
 		}
 		else
-			jump_space_and_quotes(&token);
-		ft_printf("token: %s\n", token);
+			token++;
 	}
 	return 0; // Nessun errore di sintassi rilevato
 }
