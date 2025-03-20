e31707ec535cb461eca5a640e4509971db1bc5dd
SamyBravy <samybravy@gmail.com>
2025-03-20 11:41:34 +0100

    po

--- Executer/executer.c
+++ Executer/executer.c
@@ -105,8 +105,8 @@ static int	create_pipe_and_fork(t_input **input, t_cmd *cmd, t_list **env,
 		exec_cmd(input, cmd, env);
 	}
 	close(STDIN_FILENO);
-	dup2(pipefd[0], STDIN_FILENO);
-	return (!which_builtin(*input) && close(pipefd[0]), close(pipefd[1]), pid);
+	return (dup2(pipefd[0], STDIN_FILENO), (void)(!which_builtin(*input)
+		&& close(pipefd[0])), close(pipefd[1]), pid);
 }
 
 void	*executer(t_input **input, t_list **env, int *exit_status)
